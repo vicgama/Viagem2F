3bb68eea29a4425d2180f38a43ccc592840107ea
vicgama <victoriagama80@gmail.com>
2023-09-18 09:54:05 -0300

    Adicionando cpf na pessoaTurista

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,18 @@ public class Main {
         mochileira.setCpf("456");
         // Definir comportamento
         String retorno = mochileira.viajar();
-        JOptionPane.showMessageDialog(null,retorno);
+        // Atribuindo dinâmica
+        String nome = JOptionPane.showInputDialog("Entre com o nome da pessoa Turista:");
+        // Criação do objeto do tipo Turista
+        Turista pessoaTurista = new Turista(nome);
+        // Saída de dados - nome armazenado acima
+        System.out.println(pessoaTurista.getNome());
+        // Pedir CPF e mostrar
+        String cpf = JOptionPane.showInputDialog("Entre com o CPF do Turista:");
+        // Adicionar CPF no objeto pessoaTurista
+        pessoaTurista.setCpf(cpf);
+        System.out.println(pessoaTurista.getCpf());
+
 
     }
 }
--- src/Turista.java
+++ src/Turista.java
@@ -4,9 +4,15 @@ public class Turista {
     private String cpf;
     //Construtor Padrão
     Turista(){}
-    // Construtor personalizado
+    // Sobrecarga do Construtor personalizado
     Turista(String _nome){
         this.nome = _nome;
+    }
+
+    // Mais uma sobrecarga do construtor
+    Turista(String _nome, String _cpf){
+        this.nome = _nome;
+        this.cpf = _cpf;
     }
     // Métodos
     public String viajar(){
